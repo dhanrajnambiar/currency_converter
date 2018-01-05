90c4ecd46f3d16d9153f2a61e7377fe80aa88d8a
Dhanraj.P <dhanrajp3531@gmail.com>
2018-01-05 17:38:17 +0530

    fourth

--- money_converter/money_converter/settings/production.py
+++ money_converter/money_converter/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xb4ipb@7!^6&md)38)ulm835^j4s*v-l!d%6$)#hr=8k(()j!b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','dhanrajnambiarvaishnavam.pythonanywhere.com']
 
 
 # Application definition
